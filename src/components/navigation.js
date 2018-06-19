02b5a9eb1c1b76e9a783af6f1286d868a594a3cf
muzzone <muzz0neasdqwe@gmail.com>
2018-06-19 17:04:25 +0300

    add styles

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -16,6 +16,13 @@ class Navigation extends Component {
           {this.props.children}
           {this.getMonthName(this.props.date.getMonth())}
         </div>
+        <ul className="dates">
+          <li className="dayHeader">{this.props.date.getDate()} Monday</li>
+          <li className="dayHeader">{this.props.date.addDays(1).getDate()} Tuesday</li>
+          <li className="dayHeader">{this.props.date.addDays(2).getDate()} Wednesday</li>
+          <li className="dayHeader">{this.props.date.addDays(3).getDate()} Thursday</li>
+          <li className="dayHeader">{this.props.date.addDays(4).getDate()} Friday</li>
+        </ul>
       </div>
     )
   }
--- src/components/time.js
+++ src/components/time.js
@@ -1,18 +1,6 @@
 import React, { Component } from 'react';
-// import $ from 'jquery';
 
 class Time extends Component {
-  // constructor(props) {
-  //   super(props);
-  //
-  //   this.handleClick = this.handleClick.bind(this);
-  // }
-
-  // handleClick() {
-  //   console.log(this.props.timeId);
-  //   $('#'+this.props.timeId).addClass('reserved');
-  // }
-
   render() {
     return (
       <li className="time" id={this.props.timeId}>
